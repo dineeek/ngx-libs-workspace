de173c7154df79390dd172f71b1e7ad9c0d3142f
dineeek <dineeek@gmail.com>
2022-12-02 18:25:39 +0100

    refactor & inital layout

--- libs/ngx-pass-code/src/lib/container/pass-code-container.component.ts
+++ libs/ngx-pass-code/src/lib/container/pass-code-container.component.ts
@@ -1,15 +1,27 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { PassCodeFormService } from '../form/pass-code-form.service';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'ngx-pass-code',
   templateUrl: './pass-code-container.component.html',
   styleUrls: ['./pass-code-container.component.scss'],
-  providers: [PassCodeFormService],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PassCodeContainerComponent implements OnInit {
+  @Input() passCodeLength = 0;
+
+  passCodes!: FormArray<FormControl>;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.passCodes = new FormArray(
+      Array(this.passCodeLength).fill(new FormControl(''))
+    );
+  }
 }
