8c728a168753646d1df74f03bb8a5f2348d1d671
dineeek <dineeek@gmail.com>
2023-05-19 21:50:09 +0200

    Migrate nx to latests

--- libs/ngx-pass-code/jest.config.ts
+++ libs/ngx-pass-code/jest.config.ts
@@ -3,15 +3,16 @@ export default {
   displayName: 'ngx-pass-code',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$'
-    }
-  },
+  globals: {},
   coverageDirectory: '../../coverage/libs/ngx-pass-code',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular'
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$'
+      }
+    ]
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
